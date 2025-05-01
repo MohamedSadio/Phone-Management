7364e26dbee16eb36e950a094d72de318a60f977
MohamedSadio <159494473+MohamedSadio@users.noreply.github.com>
2025-05-01 02:04:46 +0000

    base

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import HeroSection from './components/HeroSection/HeroSection';
+import NavigationBar from './components/NavigationBar/NavigationBar';
+
+function App() {
+  return (
+    <>
+      <NavigationBar/>
+      <HeroSection/>
+    </>
+  );
+}
+
+export default App;
